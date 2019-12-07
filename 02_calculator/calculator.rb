ce251c7198ef53a6fe07949f1d8c46c1241b96ac
Ubuntu <ubuntu@ip-172-31-93-199.ec2.internal>
2019-12-07 05:51:40 +0000

    Completed 3 of 7 exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+
+def add(a, b)
+    return a + b
+end 
+
+
+def subtract(a, b)
+    return a - b
+end 
+
+
+def sum(array)
+    total = 0
+    array.each do |a|
+        total = total + a
+    end 
+    return total
+end 
+
+
+def multiply(a, b) 
+    return a * b
+end 
+
+
+def power(a, b)
+    return a ** b
+end 
+
+
+def factorial(a)
+    total = 1
+    while a > 0
+        total = total * a
+        a = a - 1
+    end
+    return total
+end    
