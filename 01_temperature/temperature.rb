49fbeb90206a2db03481fda7829f79ebe63cceb9
Ubuntu <ubuntu@ip-172-31-93-199.ec2.internal>
2019-12-07 05:26:41 +0000

    Completed 2 of 7 exercises

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+
+def ftoc(fahrenheit) 
+    return celsius = (fahrenheit - 32.00) * 5.00/9.00
+end 
+
+def ctof(celsius) 
+    return fahrenheit = 9.00 * (celsius / 5.00) + 32.00
+end
