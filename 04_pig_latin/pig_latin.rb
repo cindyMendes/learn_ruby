7522d1fb787998b6680a5213afb545074216cffe
Ubuntu <ubuntu@ip-172-31-93-199.ec2.internal>
2019-12-08 17:57:58 +0000

    Completed 5 of 7 exercices

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,41 @@
-#write your code here
+def translate(string)
+    
+    words = string.split(" ")
+    
+    vowels = ["a", "e", "i", "o", "u", "y"]
+    two_letter_consonants = ["ch", "qu", "br"]
+    three_letter_consonants = ["thr", "squ", "sch"]
+    other = ["the"]
+    
+    outcome = [];
+    
+    words.each do |word|
+        if vowels.include? word[0]
+            w = word + 'ay'
+            outcome.push w
+        else
+            if three_letter_consonants.include? word[0] + word[1] + word[2]
+                first_three_letters = word.slice!(0,3)
+                w = word + first_three_letters + "ay"
+                outcome.push w
+                
+            elsif  two_letter_consonants.include? word[0] + word[1]
+                first_two_letters = word.slice!(0,2)
+                w = word + first_two_letters + 'ay'
+                outcome.push w
+                
+            elsif other.include? word[0] + word[1] + word[2]
+                first_three_letters = word[2] + word[0] + word[1]
+                w = first_three_letters + 'ay'
+                outcome.push w
+                
+            else
+                first_letter = word.slice!(0)
+                w =  word + first_letter + 'ay'
+                outcome.push w
+            end 
+    
+        end 
+    end
+    return outcome.join(" ")
+end 
