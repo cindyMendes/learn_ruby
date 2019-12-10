f202ef961e441defb947d818430b6ab367102454
Ubuntu <ubuntu@ip-172-31-93-199.ec2.internal>
2019-12-10 04:54:03 +0000

    Conpleted 6 of 7 exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
-# write your code here
+    
+  attr_accessor :title
+  
+  def title=(string)
+      words = string.capitalize.split(" ")
+      little_words = ["and", "a", "the", "from", "but", "in", "or", "at", "on", "for", "of", "an"]
+      outcome = []
+      
+      words.each do |word|
+          if little_words.include? word
+              outcome << word
+          else
+              outcome << word.capitalize
+          end
+      end
+      @title = outcome.join(" ")
+  end 
+ 
 end
+
+
+
+
